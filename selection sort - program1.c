f8fb45409148f28899f305058413092927aa6c56
Avic <173277100+developerAvic@users.noreply.github.com>
2026-02-09 13:28:24 +0530

    Create selection sort - program1.c

---
+++ selection sort - program1.c
@@ -0,0 +1,48 @@
+Program 1 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void selectionsort(int a[], int n)
+{
+    int i, j, pos, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        pos = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[pos])
+                pos = j;
+        }
+
+        if (i != pos)
+        {
+            temp = a[i];
+            a[i] = a[pos];
+            a[pos] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int *a, n, i;
+
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+
+    a = (int*) malloc(n * sizeof(int));
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    selectionsort(a, n);
+
+    for (i = 0; i < n; i++)
+        printf("%d\t", a[i]);
+
+    free(a);
+}
