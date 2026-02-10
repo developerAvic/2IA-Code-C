96f577446729724db7a3683728286aa8a79004ef
Avic <173277100+developerAvic@users.noreply.github.com>
2026-02-10 19:25:18 +0530

    Update infix to postfix - program 3.c

--- infix to postfix - program 3.c
+++ infix to postfix - program 3.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
-char istack[20];
+#define MAX 50
+
+char istack[MAX];
 int tos = -1;
 
-float stack[20];
+float stack[MAX];
 int top = -1;
 
-void ipush(char s)
-{
+/* ---------- Operator Stack ---------- */
+
+void ipush(char s) {
+    if (tos == MAX - 1)
+    {
+        printf("Operator stack overflow\n");
+        exit(1);
+    }
     istack[++tos] = s;
 }
 
-char ipop()
-{
+char ipop() {
+    if (tos == -1)
+    {
+        printf("Operator stack underflow\n");
+        exit(1);
+    }
     return istack[tos--];
 }
 
-int precd(char s)
-{
+int precd(char s) {
     switch (s)
     {
         case '^': return 4;
@@ -29,119 +41,103 @@ int precd(char s)
         case '/': return 3;
         case '+':
         case '-': return 2;
-        case '(':
-        case ')': return 1;
+        case '(' : return 1;
     }
     return 0;
 }
 
-void convertip(char infix[20], char postfix[20])
-{
-    int i, j = 0, n;
+/* ---------- Infix to Postfix ---------- */
+
+void convertip(char infix[], char postfix[]) {
+    int i, j = 0;
     char symbol;
 
     ipush('(');
-    n = strlen(infix);
-    infix[n] = ')';
-    n = n + 1;
+    strcat(infix, ")");
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; infix[i] != '\0'; i++) {
         symbol = infix[i];
 
-        switch (symbol)
-        {
-            case '(':
-                ipush(symbol);
-                break;
-
-            case ')':
-                while (istack[tos] != '(')
-                    postfix[j++] = ipop();
-                ipop(); // pop '('
-                break;
-
-            case '^':
-            case '*':
-            case '/':
-            case '%':
-            case '+':
-            case '-':
-                while (precd(symbol) <= precd(istack[tos]))
-                    postfix[j++] = ipop();
-                ipush(symbol);
-                break;
-
-            default:
-                postfix[j++] = symbol;
+        if (isalnum(symbol)) {
+            postfix[j++] = symbol;
+        }
+        else if (symbol == '(') {
+            ipush(symbol);
+        }
+        else if (symbol == ')') {
+            while (istack[tos] != '(')
+                postfix[j++] = ipop();
+            ipop();
+        }
+        else {
+            while (precd(symbol) <= precd(istack[tos]))
+                postfix[j++] = ipop();
+            ipush(symbol);
         }
     }
 
     postfix[j] = '\0';
 }
 
-void push(float ele)
-{
+/* ---------- Operand Stack ---------- */
+
+void push(float ele) {
+    if (top == MAX - 1) {
+        printf("Evaluation stack overflow\n");
+        exit(1);
+    }
     stack[++top] = ele;
 }
 
-float pop()
-{
+float pop() {
+    if (top == -1) {
+        printf("Evaluation stack underflow\n");
+        exit(1);
+    }
     return stack[top--];
 }
 
-void evaluate(char postfix[50])
-{
-    int i;
-    char sym;
-    float op1, op2, result, x;
+/* ---------- Postfix Evaluation ---------- */
 
-    for (i = 0; i < strlen(postfix); i++)
-    {
-        sym = postfix[i];
+void evaluate(char postfix[]){
+    float op1, op2, x;
 
-        if (isalpha(sym))
-        {
-            printf("\nEnter the value for %c:\t", sym);
+    for (int i = 0; postfix[i] != '\0'; i++){
+        char sym = postfix[i];
+
+        if (isalpha(sym)) {
+            printf("Enter value of %c: ", sym);
             scanf("%f", &x);
             push(x);
         }
-        else
-        {
+        else {
             op2 = pop();
             op1 = pop();
 
-            switch (sym)
-            {
+            switch (sym){
                 case '+': push(op1 + op2); break;
                 case '-': push(op1 - op2); break;
                 case '*': push(op1 * op2); break;
                 case '/': push(op1 / op2); break;
                 case '%': push((int)op1 % (int)op2); break;
                 case '^': push(pow(op1, op2)); break;
-
                 default:
-                    printf("Invalid postfix expression");
-                    exit(0);
+                    printf("Invalid postfix expression\n");
+                    exit(1);
             }
         }
     }
-
-    result = pop();
-    printf("The result is %f\n", result);
+    printf("Result = %.2f\n", pop());
 }
 
-int main()
-{
-    char infix[20], postfix[20];
 
-    printf("\nEnter infix expression:\n");
-    gets(infix);
-
+int main(){
+    char infix[MAX], postfix[MAX];
+    printf("Enter infix expression: ");
+    fgets(infix, MAX, stdin);
+    infix[strcspn(infix, "\n")] = '\0';
     convertip(infix, postfix);
-    printf("Postfix expression is\n%s", postfix);
-
+    printf("Postfix Expression: %s\n", postfix);
     evaluate(postfix);
-
     return 0;
 }
